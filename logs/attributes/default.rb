38f3195d99080d2371bf2ef6a361bc2938a408a5
Ben Bridts <ben.bridts@gmail.com>
2015-08-07 16:41:27 +0200

    Create state file directory for awslogs

--- logs/attributes/default.rb
+++ logs/attributes/default.rb
@@ -1,1 +1,3 @@
 default[:cwlogs][:logfile] = '/var/log/aws/opsworks/opsworks-agent.log'
+default[:cwlogs][:state_file_dir] = '/var/awslogs/state'
+default[:cwlogs][:state_file_name] = 'agent-state'
--- logs/recipes/default.rb
+++ logs/recipes/default.rb
@@ -2,6 +2,14 @@ package 'awslogs' do
   action :install
 end
 
+directory node[:cwlogs][:state_file_dir] do
+  owner 'root'
+  group 'root'
+  mode 0755
+  recursive true
+  action :create
+end
+
 template '/etc/awslogs/awscli.conf' do
   cookbook 'logs'
   source 'awscli.conf.erb'
