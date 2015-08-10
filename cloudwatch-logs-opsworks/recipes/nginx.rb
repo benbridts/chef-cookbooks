b8ee1fb68608b5742322ee530dae1ccce253baf7
Ben Bridts <ben.bridts@gmail.com>
2015-08-10 12:53:07 +0200

    Syntax error

--- cloudwatch-logs-opsworks/recipes/nginx.rb
+++ cloudwatch-logs-opsworks/recipes/nginx.rb
@@ -24,7 +24,7 @@ if node['nginx']['log_dir']
     log_group_name = "#{node['cwlogs_ow']['base_name']}/nginx/#{app['name']}/access.log"
     node.default['cwlogs']['logfiles'][log_group_name] = {
       :log_stream_name => '{instance_id}-{hostname}',
-      :log_group_name => log_group_name
+      :log_group_name => log_group_name,
       :file => "#{node['nginx']['log_dir']}/#{app['name'].gsub('-', '_')}.access.log",
       :datetime_format => '[%d/%b/%Y:%H:%M:%S %z]',
     }
