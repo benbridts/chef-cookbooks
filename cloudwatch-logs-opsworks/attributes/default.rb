38027540284848d4ccef49d923566bf537837479
Ben Bridts <ben.bridts@gmail.com>
2015-08-10 14:55:43 +0200

    Improve installer log

--- cloudwatch-logs-opsworks/attributes/default.rb
+++ cloudwatch-logs-opsworks/attributes/default.rb
@@ -14,6 +14,7 @@ log_group_name = "#{default[:cwlogs_ow][:base_name]}/ow/installer.log"
 default["cwlogs"]["logfiles"][log_group_name] = {
     :log_stream_name => '{instance_id}-{hostname}',
     :log_group_name => log_group_name,
-    :file => '/var/log/aws/opsworks/opsworks-agent.log',
+    :file => '/var/log/aws/opsworks/installer.log',
     :datetime_format => "[%Y-%m-%d %H:%M:%S]",
+    :multi_line_start_pattern = '{datetime_format}'
 }
