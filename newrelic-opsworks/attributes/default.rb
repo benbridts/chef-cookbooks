94ffdf8eb8368bb35dac0f17170bfe69aa404904
Ben Bridts <ben.bridts@gmail.com>
2015-08-13 17:50:56 +0200

    Trigger restart

--- newrelic-opsworks/attributes/default.rb
+++ newrelic-opsworks/attributes/default.rb
@@ -1,6 +1,9 @@
-
+# Include stack and instance id in newrelic hostname
 default['newrelic']['server_monitoring']['hostname'] = [
   node['opsworks']['stack']['name'],
   node['opsworks']['instance']['hostname'],
   node['opsworks']['instance']['aws_instance_id']
 ].join('_')
+
+# Set service name so we can trigger a restart
+default['newrelic']['server_monitor_agent']['service_name'] = 'newrelic-sysmond'
--- newrelic-opsworks/recipes/server_monitor_agent.rb
+++ newrelic-opsworks/recipes/server_monitor_agent.rb
@@ -1,7 +1,14 @@
+# Service created by included recipe
+service node['newrelic']['server_monitor_agent']['service_name'] do
+  supports [:start, :stop, :status, :restart, :reload]
+  action :nothing
+end
+
 include_recipe 'newrelic::server_monitor_agent'
 
 group 'docker' do
   append true
   members ['newrelic']
   action :manage
+  notifies :restart, "service[#{node['newrelic']['server_monitor_agent']['service_name']}]", :delayed
 end
